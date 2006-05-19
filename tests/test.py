78610077c0e10a464fd9801b4d2321939eb6ca0e
jerome <alet@librelogiciel.com>
2006-05-19 14:10:41 +0000

    Added test script

---
+++ tests/test.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: ISO-8859-15 -*-
+#
+# ipplib : IPP and CUPS support for Python
+#
+# (c) 2003, 2004, 2005, 2006 Jerome Alet <alet@librelogiciel.com>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# $Id$
+
+import sys
+
+sys.path.insert(0, "../ipplib")
+import ipplib        
+    
+cups = ipplib.CUPS()    
+answer = cups.getJobAttributes(566)
+print answer
+#print answer.operation["attributes-charset"]
+
