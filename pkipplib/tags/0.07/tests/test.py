84ca501c0edd9cbea559e196a81d1aa4b7073e77
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2006-06-26 07:34:46 +0000

    Tag for v0.07

---
+++ pkipplib/tags/0.07/tests/test.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: ISO-8859-15 -*-
+#
+# pkipplib : IPP and CUPS support for Python
+#
+# (c) 2003, 2004, 2005, 2006 Jerome Alet <alet@librelogiciel.com>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# $Id$
+
+import sys
+
+sys.path.insert(0, "../pkipplib")
+import pkipplib        
+    
+cups = pkipplib.CUPS()
+answer = cups.createSubscription("ipp://localhost/", ["printer-added", "printer-deleted"],
+                                                     userdata="samplenotifier:blah",
+                                                     recipient="samplenotifier",
+                                                     charset="utf-8")
+print answer
+
+#print answer.operation["attributes-charset"]
+
